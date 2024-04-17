fe4b9ed41b8ad7d0baca6aa20474506be980fc53
Sofiene Gallalou <sofiene.gallalou@sofrecom.com>
2024-04-17 12:26:13 +0100

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gallalou123:gallalou@cluster0.hodopj1.mongodb.net/')
+mongoose.connect('mongodb+srv://you-username:your-password@cluster0.hodopj1.mongodb.net/')
 .then( () => { 
     console.log("Connexion DB OK");
     const server = app.listen(3000, () => {
